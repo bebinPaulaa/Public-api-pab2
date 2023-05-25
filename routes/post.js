ae79c4afc1fd867f7329da407e7e5cdd404f156d
bebinPaulaa <bebinpaula11@gmail.com>
2023-05-25 12:53:20 +0700

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -1,15 +1,33 @@
 const express = require('express')
-const post = require('../models/post')
+const Post = require('../models/post')
 const router = express.Router()
 
 router.get('/', async(req, res) => {
     try {
-        const Post = await post.find()
-        res.json(Post)
+        const post = await Post.find()
+        res.json(post)
     } catch (error) {
         res.json({
             message: error
         })
+    }
+})
+
+router.post('/', async (req, res) => {
+    const inputPost = new Post({
+        content: req.body.content,
+        user_id: req.body.user_id,
+        username: req.body.username
+    })
+
+    try {
+        const post = await inputPost.save()
+        res.json(post)
+    }catch (error) {
+        res.json({
+            message: error
+        })
+    
     }
 })
 
